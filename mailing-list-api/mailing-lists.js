b7cc5670eaef713a24cb039b8a46f6cca6deec45
MitchLloyd <mitchelllloyd154@gmail.com>
2023-07-02 17:18:42 +0100

    Create mailing-lists.js

---
+++ mailing-list-api/mailing-lists.js
@@ -0,0 +1,9 @@
+{
+    staff: ["talea@techtonica.org", "michelle@techtonica.org"],
+    "cohort-h1-2020": [
+      "ali@techtonica.org",
+      "humail@techtonica.org",
+      "khadar@techtonica.org",
+    ],
+    "tech-ed" : ["mitch@curriculum.org", "sally@curriculum.org"]
+}
